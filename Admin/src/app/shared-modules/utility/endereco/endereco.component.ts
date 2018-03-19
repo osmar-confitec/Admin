522f5f0d830726b5587b5bf96702779ac1b1f096
Osmar <osmargv99@gmail.com>
2018-03-18 22:25:08 -0300

    primeiro commit

---
+++ Admin/src/app/shared-modules/utility/endereco/endereco.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'projeto-admin-endereco',
+  templateUrl: './endereco.component.html',
+  styleUrls: ['./endereco.component.css']
+})
+export class EnderecoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
